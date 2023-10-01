165f666b5bf712cd2d1f5069fe6168ad47631d1d
hunit <hunit612@naver.com>
2023-10-02 00:32:32 +0900

    api swagger 추가

--- src/users/dto/join.request.dto.ts
+++ src/users/dto/join.request.dto.ts
@@ -1,7 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class JoinRequestDto {
+  @ApiProperty({
+    example: 'hunit612@gmail.com',
+    description: 'email',
+    required: true,
+  })
   public email: string;
 
+  @ApiProperty({
+    example: 'hunit612',
+    description: 'nickname',
+    required: true,
+  })
   public nickname: string;
 
+  @ApiProperty({
+    example: 'test1234',
+    description: 'password',
+    required: true,
+  })
   public password: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,30 +1,49 @@
-import { Body, Controller, Get, Post, Req, Res } from "@nestjs/common";
-import { JoinRequestDto } from "./dto/join.request.dto";
-import { UsersService } from "./users.service";
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { JoinRequestDto } from './dto/join.request.dto';
+import { UsersService } from './users.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserDto } from '../common/dto/user.dto';
 
+@ApiTags('USER')
 @Controller('api/users')
 export class UsersController {
-  constructor(private usersService: UsersService) {
-  }
+  constructor(private usersService: UsersService) {}
+  @ApiResponse({
+    type: UserDto,
+  })
+  @ApiOperation({ summary: '내정보조회' })
   @Get()
   getUsers(@Req() req) {
     return req.user;
   }
 
+  @ApiOperation({ summary: '회원가입' })
   @Post()
   postUsers(@Body() data: JoinRequestDto) {
     this.usersService.postUsers(data.email, data.nickname, data.password);
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'success',
+    type: UserDto,
+  })
+  @ApiResponse({
+    status: 500,
+    description: '서버에러',
+    type: UserDto,
+  })
+  @ApiOperation({ summary: '로그인' })
   @Post('login')
   logIn(@Req() req) {
     return req.user;
   }
 
+  @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
   logOut(@Req() req, @Res() res) {
     req.logOut();
-    res.clearCookie('connect.sid', {httpOnly: true});
+    res.clearCookie('connect.sid', { httpOnly: true });
     res.send('ok');
   }
 }
