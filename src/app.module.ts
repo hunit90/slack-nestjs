155bc56d9655a3a7e24a82c99ce106569dee4eb9
hunit <hunit612@naver.com>
2023-10-18 00:11:47 +0900

    typeorm entitiy 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,26 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { ChannelsModule } from './channels/channels.module';
 import { DmsModule } from './dms/dms.module';
+import { UsersService } from './users/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as process from 'process';
+import { Users } from './entities/Users';
+import { ChannelChats } from './entities/ChannelChats';
+import { ChannelMembers } from './entities/ChannelMembers';
+import { Channels } from './entities/Channels';
+import { DMs } from './entities/DMs';
+import { Mentions } from './entities/Mentions';
+import { WorkspaceMembers } from './entities/WorkspaceMembers';
+import { Workspaces } from './entities/Workspaces';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     UsersModule,
     WorkspacesModule,
     ChannelsModule,
@@ -22,22 +28,32 @@ import * as process from 'process';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
+      port: 3306,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      autoLoadEntities: true,
+      entities: [
+        ChannelChats,
+        ChannelMembers,
+        Channels,
+        DMs,
+        Mentions,
+        Users,
+        WorkspaceMembers,
+        Workspaces,
+      ],
       keepConnectionAlive: true,
       synchronize: false,
       logging: true,
-      charset: 'utf8mb4',
+      charset: 'utf8mb4_general_ci',
     }),
     TypeOrmModule.forFeature([Users]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer): any {
+export class AppModule {
+  configure(consumer: MiddlewareConsumer): void {
     consumer.apply(LoggerMiddleware).forRoutes('*');
   }
 }
